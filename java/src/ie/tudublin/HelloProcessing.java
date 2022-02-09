1b4b7f0da262f55b27798c0888a4016cfe16ec80
imraghavbnsal007 <raghavb025@gmail.com>
2022-02-09 17:39:37 +0000

    lab3

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -7,61 +7,90 @@ public class HelloProcessing extends PApplet
 
 	public void settings()
 	{
-		size(500, 500);
+		size(1000, 1000);
 	}
 
 	public void setup() {
-		colorMode(HSB);
-		background(0);
+		colorMode(RGB);
+		// background(0);
 
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
+		// x1 = random(0, width);
+		// x2 = random(0, width);
+		// y1 = random(0, height);
+		// y2 = random(0, height);
 
-		float range = 5;
+		// float range = 5;
 
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
+		// x1dir = random(-range, range);
+		// x2dir = random(-range, range);
+		// y1dir = random(-range, range);
+		// y2dir = random(-range, range);
 
-		smooth();
+		// smooth();
 		
 	}
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+	// float x1, y1, x2, y2;
+	// float x1dir, x2dir, y1dir, y2dir;
+	// float c = 0;
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
+	// 	strokeWeight(2);
+	// 	stroke(c, 255, 255);
+	// 	c = (c + 1f) % 255;
+	// 	line(x1, y1, x2, y2);
 
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
+	// 	x1 += x1dir;
+	// 	x2 += x2dir;
+	// 	y1 += y1dir;
+	// 	y2 += y2dir;
 		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
+	// 	if (x1 < 0 || x1 > width)
+	// 	{
+	// 		x1dir = - x1dir;
+	// 	}
+	// 	if (y1 < 0 || y1 > height)
+	// 	{
+	// 		y1dir = - y1dir;
+	// 	}
 
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+	// 	if (x2 < 0 || x2 > width)
+	// 	{
+	// 		x2dir = - x2dir;
+	// 	}
+	// 	if (y2 < 0 || y2 > height)
+	// 	{
+	// 		y2dir = - y2dir;
+	// 	}
+	stroke(255);
+	background(255, 0, 0); //greyscale
+	// line (10, 10, 200, 200); //x1, y1, x2, y2
+	fill(225,225,51);
+	circle (450, 450, 7   00); //cx, cy, r
+	noFill();
+
+	// ellipse(50, 200, 90, 200); //cx, cy, width, height
+
+	// stroke(255);
+	fill (137,207,240);
+	// noFill();
+	// noStroke();
+	// rectMode(CENTER);
+	// rect(300, 20, 100, 20); //Topleftx, toplefty, widthm height
+
+	// rectMode(CORNER);
+	// rect(200, 20, 100, 20);
+
+	// point(20, 300);
+
+	triangle(15, 400, 490, 400, 250, 5); //x1, y1, x2, y2, x3, y3
+	noFill();
+	fill(244, 194, 194);
+	ellipse(250, 250, 200, 90);
+	noFill();
+	fill(0);
+	circle(250, 250, 75);
+
 	}
 }
--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -6,12 +6,17 @@ public class Loops extends PApplet
 {
 
 
-	int mode = 0;
-
+	
 	public void settings()
 	{
 		size(500, 500);
+		cx = width/2;
+		cy = height /2;
 	}
+	int mode = 0;
+	float cx;
+	float cy;
+
 
 	public void setup() {
 		colorMode(HSB);
@@ -30,13 +35,57 @@ public class Loops extends PApplet
 	
 	public void draw()
 	{
-		
+		background(0);
+		noStroke();
 		switch(mode)	
 		{
 			case 0:
-				break;
+			fill(50,255,255);
+			if (mouseX < cx)
+			{
+				
+				rect(0,0,cx,height);
+			}
+			else
+			{
+				rect(cx, 0 , cx, height);
+			}
+			
+			break;
 			case 1:
+			fill(50,255,255);
+				if (mouseX < cx && mouseY < cy)
+				{
+					rect(0, 0, cx, cy);
+				}
+
+				else if (mouseX > cx && mouseY < cy)
+				{
+					rect(cx, 0, cx, cy);
+				}
+				else if (mouseX <cx && mouseY > cy )
+				{
+					rect(0, cy, cx, cy);
+				}
+				else
+				{
+					rect(cx, cy, cx, cy);
+				}
 				break;
+				case 2:
+				{
+					// int numRects = (int)(mouseX / 10.0f);
+					int numRects = 10;
+                	float w = width / (float) numRects;
+                	float cgap = 255 / (float) numRects;
+                	for(int i = 0 ; i < numRects ; i ++)
+                	{
+                    	fill(i * cgap, 255, 255);
+                    	rect(i * w, 0, w, height);
+                	}
+                	break;
+					
+				}
 		}
 	}
 }
