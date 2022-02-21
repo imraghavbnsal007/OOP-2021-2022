b4122f26eb873884f25da79e538bd8eb4ab32779
imraghavbnsal007 <raghavb025@gmail.com>
2022-02-21 13:02:11 +0000

    arrays

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -12,8 +12,8 @@ public class Arrays extends PApplet {
     //other way of writing arrays in java
     float[] a1 = new float[100];
     float[] a2; //Null pointer exception, if we try to access the element of a2, in java pointer called refrence. 
-    // float MAX_VALUE =0;
-    // float MIN_VALUE = 0;
+    int MAX_VALUE = 0;
+        int MIN_VALUE= 0;
     public void settings()
     {
         size(500, 500);
@@ -43,8 +43,7 @@ public class Arrays extends PApplet {
             println(rainfall[i] + "\t" + months[i]); 
         }
         
-        int MAX_VALUE = 0;
-        int MIN_VALUE= 0;
+        
 
         for(int i = 0; i <rainfall.length; i++ )
         {
@@ -75,8 +74,33 @@ public class Arrays extends PApplet {
 
     public void draw()
     {
-        
+        // //sets location for each bar, 50 units
+        // for(int i=0;i<500;i=i+50) {
+        //     //creates a counter variable for each values in the array
+        //     for(int x= 0; x < rainfall.length; x++) {
+        //         //draws rectangle for each of the values in the array to correspomding height
+        //         rect(i, 475, 30, -rainfall[x], 1);
+        //     }
+        // }
+        background(0);
+        colorMode(HSB);
+        float w = width / (float)rainfall.length;
+        for (int i =0; i <rainfall.length; i++)
+        {
+            // float x = i * w;
+            float x = map(i, 0, rainfall.length, 0, width);
 
+            int c = (int)map(i, 0, rainfall.length, 0, 255);
+            fill(c, 255, 255);
+            float h = map(rainfall[i], 0, rainfall[MAX_VALUE], 0, -height);
+            rect(x, height, w, -rainfall[i]);
+            fill(255);
+            textAlign(CENTER, CENTER);
+            text(months[i], x + (w/2), height - 50);
+            //map suing
+            
+        }
+    
     }
     
 }
