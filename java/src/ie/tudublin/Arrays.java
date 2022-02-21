e1016ba8f702ed2d1ef3d822f14edefb4f7c4b86
imraghavbnsal007 <raghavb025@gmail.com>
2022-02-21 11:32:40 +0000

    arrays

---
+++ java/src/ie/tudublin/Arrays.java
@@ -0,0 +1,21 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Arrays extends PApplet {
+    public void settings()
+    {
+        size(500, 500);
+    }
+
+    public void setup()
+    {
+
+    }
+
+    public void draw()
+    {
+        
+    }
+    
+}
