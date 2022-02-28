83a291c1940bba85ecf39402f11dd9a80b02ce63
imraghavbnsal007 <raghavb025@gmail.com>
2022-02-28 12:52:14 +0000

    Audio1

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -1,41 +1,57 @@
 package ie.tudublin;
 
-import ddf.minim.AudioBuffer;
-import ddf.minim.AudioInput;
-import ddf.minim.AudioPlayer;
-import ddf.minim.Minim;
 import processing.core.PApplet;
 
-public class Audio1 extends PApplet
-{
+//Sound imports
+import ddf.minim.Minim;
+import ddf.minim.AudioPlayer;
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+
+public class Audio1 extends PApplet {
     Minim minim;
     AudioPlayer ap;
     AudioInput ai;
     AudioBuffer ab;
 
-    public void settings()
-    {
-        size(1024, 500);
-    }
+    float y = 0;
 
-    public void setup()
-    {
+    public void settings() {
+        size(1024, 500);
+    }// end settings()
+
+    public void setup() {
         minim = new Minim(this);
         ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-        ab = ai.mix; 
-    }
+        ab = ai.mix;
+        colorMode(HSB);
+        // colorMode(RGB);\
 
-    public void draw()
-    {
+        y = height/2;
+    }// end setup()
+
+    public void draw() {
         background(0);
         stroke(255);
         float halfH = height / 2;
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            line(i, halfH, i, halfH + ab.get(i) * halfH);
-        }
-    }
-    
+        float avg = 0;
+        float sum = 0;
 
-    
+        for (int i = 0; i < ab.size(); i = i + 5) {
+            // float c = map(ab.get(i), -1, 1, 0, 255);
+            float c = map(i, 0, ab.size(), 0, 255);
+            stroke(c, 255, 255);
+            // line(i, halfH, i, halfH + ab.get(i) * halfH);
+            sum += abs(ab.get(i));
+        }
+
+        avg = sum / (float) ab.size();
+
+        stroke(255);
+        fill(100, 255, 255);
+        circle(width / 2, halfH, avg * 900);
+        circle(100, y, 50);
+        y += random(-10, 10);
+
+    }// end draw()
 }
