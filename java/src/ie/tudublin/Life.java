fc0dd6aa3df64dfb1d1b3be761f5eda388154dc0
imraghavbnsal007 <raghavb025@gmail.com>
2022-03-07 13:31:30 +0000

    life

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -3,22 +3,25 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class Life extends PApplet {
-
+	
 	LifeBoard board;
 
+
 	public void settings() {
-		size(500, 500);
-		board = new LifeBoard(100, this);
+		size(1000, 1000);
+		board = new LifeBoard(200, this);
 		board.randomise();
+		println(board.countCellsAround(1, 1));
+
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 
 	}
-
 	
 	public void draw() {
 		board.render();
+		board.update();
 	}
 }
